7d7e0603f3d9ce32e5972395f106c7c1aa40694e
Timothy <t.m.martel@gmail.com>
2017-09-26 09:16:35 -0400

    uploaded files

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + "/public"));
+
+app.get('/', function (req, res){
+  res.render("index");
+});
+
+app.listen(process.env.PORT, function () {
+  console.log('Server is listening ...');
+});
